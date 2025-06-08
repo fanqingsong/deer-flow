1669dee408499d39eb7b4bf5ea317e91145d5263
Henry Li <henry1943@163.com>
2025-06-08 19:41:12 +0800

    fix: fix importing issues

--- web/src/components/magicui/border-beam.tsx
+++ web/src/components/magicui/border-beam.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "~/lib/utils";
-import { motion, MotionStyle, Transition } from "motion/react";
+import { motion, type MotionStyle, type Transition } from "motion/react";
 
 interface BorderBeamProps {
   /**
@@ -59,7 +59,7 @@ export const BorderBeam = ({
   initialOffset = 0,
 }: BorderBeamProps) => {
   return (
-    <div className="pointer-events-none absolute inset-0 rounded-[inherit] border border-transparent [mask-clip:padding-box,border-box] [mask-composite:intersect] [mask-image:linear-gradient(transparent,transparent),linear-gradient(#000,#000)]">
+    <div className="pointer-events-none absolute inset-0 rounded-[inherit] border border-transparent [mask-image:linear-gradient(transparent,transparent),linear-gradient(#000,#000)] [mask-composite:intersect] [mask-clip:padding-box,border-box]">
       <motion.div
         className={cn(
           "absolute aspect-square",
